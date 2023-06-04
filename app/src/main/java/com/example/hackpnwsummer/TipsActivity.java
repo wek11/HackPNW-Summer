9d81e4e037a4098320ecc72b92bbc9452bea6b3d
DoggoOnBongo <dennonhart@gmail.com>
2023-06-04 10:46:24 -0700

    commit

--- app/src/main/java/com/example/hackpnwsummer/TipsActivity.java
+++ app/src/main/java/com/example/hackpnwsummer/TipsActivity.java
@@ -2,7 +2,11 @@ package com.example.hackpnwsummer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TipsActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class TipsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tips);
+
+        Button button = (Button)findViewById(R.id.donateButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://www.stackoverflow.com/"));
+                startActivity(viewIntent);
+            }
+        });
     }
 }
